12a7d2532c1b0f872981483d0cd7ccbc45a2a403
root <root@localhost.localdomain>
2019-04-02 09:43:13 -0300

    pdf meta data reader

---
+++ violent python/pdfreader.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import pyPdf2
+import optparse
+from pyPdf2 import PdfFileReader
+
+def printMeta(fileName):
+    pdfFile = PdfFileReader(file(fileName, 'rb'))
+    docInfo = pdfFile.getDocumentInfo()
+    print '[+] ***** PDF Meta Data *****'
+    print '[+] #####'+fileName +'#####'
+    for metaItem in docInfo:
+        print '[+] '+metaItem+':'+docInfo[metaItem]
+
+def main():
+    parser = optparse.OptionParser('USage: %prog "+\" -f <pdf file name>')
+    parser.add_option("-f", "--file", dest=fileName, help="specify a PDF to get the metadata from")
+    (options, args) = parser.parse_args()
+    fileName = options.fileName
+    if fileName == None:
+         print parser.USage
+         exit(0)
+    else:
+        printMeta(fileName)
+if __NAME__ == '__main__':
+    main()
